60f9759fe4b21488f67b473badf5df24a2942760
theLazyBoy_mmTu <114607201+zanzandora@users.noreply.github.com>
2025-06-16 14:09:07 +0700

    fet: Refactor routing logic to enhance authentication flow and remove unused PrivateRoute component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,6 @@ import NotificationPage from './pages/NotificationPage';
 import CallPage from './pages/CallPage';
 import ChatPage from './pages/ChatPage';
 import OnBoardingPage from './pages/OnBoardingPage';
-import PrivateRoute from './components/ProtectedRoute ';
 import Loader from './components/Loader';
 import useAuthUser from './hooks/useAuthUser';
 import { Toaster } from 'react-hot-toast';
@@ -15,7 +14,6 @@ function App() {
   const { isLoading, authUser } = useAuthUser();
 
   const isAuthenticated = Boolean(authUser);
-
   const isOnBoarded = authUser?.inOnboarded;
 
   if (isLoading) {
@@ -26,35 +24,81 @@ function App() {
     <div className='h-screen ' data-theme='dracula'>
       <Routes>
         <Route
+          path='/'
+          element={
+            isAuthenticated && isOnBoarded ? (
+              <HomePage />
+            ) : (
+              <Navigate to={!isAuthenticated ? '/login' : '/onboarding'} />
+            )
+          }
+        />
+        <Route
           path='/signup'
           element={
-            !isAuthenticated ? <SignUpPage /> : <Navigate to='/' replace />
+            !isAuthenticated ? (
+              <SignUpPage />
+            ) : (
+              <Navigate to={isOnBoarded ? '/' : '/onboarding'} />
+            )
           }
         />
         <Route
           path='/login'
           element={
-            !isAuthenticated ? <LoginPage /> : <Navigate to='/' replace />
+            !isAuthenticated ? (
+              <LoginPage />
+            ) : (
+              <Navigate to={isOnBoarded ? '/' : '/onboarding'} />
+            )
+          }
+        />
+        <Route
+          path='/notifications'
+          element={
+            isAuthenticated && isOnBoarded ? (
+              <NotificationPage />
+            ) : (
+              <Navigate to={!isAuthenticated ? '/login' : '/onboarding'} />
+            )
+          }
+        />
+        <Route
+          path='/call/:id'
+          element={
+            isAuthenticated && isOnBoarded ? (
+              <CallPage />
+            ) : (
+              <Navigate to={!isAuthenticated ? '/login' : '/onboarding'} />
+            )
           }
         />
 
-        {/* Sử dụng PrivateRoute làm route cha để bảo vệ các route con */}
-        <Route element={<PrivateRoute authUser={authUser} />}>
-          <Route
-            path='/'
-            element={
-              isAuthenticated && isOnBoarded ? (
-                <HomePage />
+        <Route
+          path='/chat/:id'
+          element={
+            isAuthenticated && isOnBoarded ? (
+              <ChatPage />
+            ) : (
+              <Navigate to={!isAuthenticated ? '/login' : '/onboarding'} />
+            )
+          }
+        />
+
+        <Route
+          path='/onboarding'
+          element={
+            isAuthenticated ? (
+              !isOnBoarded ? (
+                <OnBoardingPage />
               ) : (
-                <Navigate to={!isAuthenticated ? '/login' : '/onboarding'} />
+                <Navigate to='/' />
               )
-            }
-          />
-          <Route path='/notification' element={<NotificationPage />} />
-          <Route path='/call' element={<CallPage />} />
-          <Route path='/chat' element={<ChatPage />} />
-          <Route path='/onboarding' element={<OnBoardingPage />} />
-        </Route>
+            ) : (
+              <Navigate to='/login' />
+            )
+          }
+        />
       </Routes>
 
       <Toaster />
